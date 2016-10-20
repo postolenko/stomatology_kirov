4ef456da9880442324b8b2e8846129f6d3fc5e8b
postolenko <denis.postolenko@yandex.ua>
2016-10-20 08:37:23 +0300

    Stomatology Kirov

--- js/scripts.js
+++ js/scripts.js
@@ -12,9 +12,19 @@ $(document).ready(function() {
 
     // });
 
+    var w = window,
+    d = document,
+    e = d.documentElement,
+    g = d.getElementsByTagName('body')[0],
+    bodyWidth = w.innerWidth || e.clientWidth || g.clientWidth;
+
     var countHeadersRow = $(".header .header-row").length - 1;
     var countHeadersRowFor = 0;
     var headersRowsHeight = 0;
+
+
+
+   
 
 
     getLogoBoxHeight();
@@ -33,10 +43,137 @@ $(document).ready(function() {
             
         }
 
+        if(bodyWidth <= 480) {
+
+            $(".sidebar-left-nav").addClass("withscroll");
+
+        } else {
+
+            $(".sidebar-left-nav").removeClass("withscroll");
+
+        }
+
         getLogoBoxHeight();
 
 
+        // for( doctoPhotoCountFor = 0; doctoPhotoCountFor >= doctoPhotoCount;  doctoPhotoCountFor++ ) {
+
+        //     if ( $(".doctor-card .photo-box:eq("+ doctoPhotoCountFor +") img").height() < $(".doctor-card .photo-box:eq("+ doctoPhotoCountFor +")").height() ) {
+
+        //         $(".doctor-card .photo-box:eq("+ doctoPhotoCountFor +") img").css({
+        //                                                                             "height" : "100%",
+        //                                                                             "width" : "auto"
+        //                                                                         });
+        //     }
+
+        // }
+
+
     });
+
+
+
+    // -----------------------------------------------------------------------------
+
+    //  Показать скрыть диалоговое окно
+
+    var doctorPhotoCount = $(".doctor-card .photo-box").length - 1;
+    var doctorPhotoCountFor = 0;
+
+    
+    var widthImg = 1;
+
+    // console.log(doctorPhotoCountFor +"  "+ doctorPhotoCount);
+
+    // for( doctorPhotoCountFor = 0; doctorPhotoCountFor <= doctorPhotoCount;  doctorPhotoCountFor++ ) {
+
+    //     // console.log(doctorPhotoCountFor +"  "+ doctorPhotoCount);
+
+
+    //     if ( $(".doctor-card .photo-box:eq("+ doctorPhotoCountFor +") img").height() < $(".doctor-card .photo-box:eq("+ doctorPhotoCountFor +")").height() ||
+    //          $(".doctor-card .photo-box:eq("+ doctorPhotoCountFor +") img").width() < $(".doctor-card .photo-box:eq("+ doctorPhotoCountFor +")").width() ) {
+
+    //         console.log($(".doctor-card .photo-box:eq("+ doctorPhotoCountFor +") img").width());
+                
+    //         // for( widthImg = 100;  ; widthImg++ ) {
+
+    //         // while ($(".doctor-card .photo-box:eq("+ doctorPhotoCountFor +") img").height() > $(".doctor-card .photo-box:eq("+ doctorPhotoCountFor +")").height() &&
+    //         //        $(".doctor-card .photo-box:eq("+ doctorPhotoCountFor +") img").width() > $(".doctor-card .photo-box:eq("+ doctorPhotoCountFor +")").width()){
+
+
+    //         //     $(".doctor-card .photo-box:eq("+ doctorPhotoCountFor +") img").css({"width" : widthImg + "%"});
+
+
+    //         // }
+
+    //         widthImg = 100;
+
+    //         var imgWidthAnim;
+
+    //         imgWidthAnim = setTimeout(function() {
+
+    //             // console.log($(".doctor-card .photo-box:eq("+ doctorPhotoCountFor +") img").width());
+
+    //             $(".doctor-card .photo-box:eq("+ 1 +") img").css({"width" : ( widthImg++ )  + "%"});
+
+    //             // widthImg++;
+
+    //             console.log($(".doctor-card .photo-box:eq("+ 1 +") img").height() +"   "+ $(".doctor-card .photo-box:eq("+ 1 +")").height() );
+
+    //             console.log($(".doctor-card .photo-box:eq("+ 1 +") img").width() +"   "+ $(".doctor-card .photo-box:eq("+ 1 +")").width());
+
+
+    //             if ( ( $(".doctor-card .photo-box:eq("+ 1 +") img").height() >= $(".doctor-card .photo-box:eq("+ 1 +")").height() ) && (  $(".doctor-card .photo-box:eq("+ 1 +") img").width() >= $(".doctor-card .photo-box:eq("+ 1 +")").width() ) ){
+
+
+    //                 // $(".doctor-card .photo-box:eq("+ doctorPhotoCountFor +") img").css({"width" : widthImg + "%"});
+
+    //                 console.log($(".doctor-card .photo-box:eq("+ 1 +") img").height() +"   "+ $(".doctor-card .photo-box:eq("+ 1 +")").height()); 
+
+    //                 clearInterval(imgWidthAnim);
+
+    //                 function getImgSize() {
+
+    //                     getImgSizeInterval();
+
+    //                 };
+
+    //             }
+
+    //             console.log($(".doctor-card .photo-box:eq("+ 1 +") img").height() +"  "+ $(".doctor-card .photo-box:eq("+ 1 +")").height());   
+
+    //         }, 100);        
+
+
+    //     }
+
+    // }
+
+    // imgSizeInterval();
+
+    // getImgSize();
+
+    // function getImgSize() {        
+
+    //     imgSizeInterval();
+
+    //     console.log(widthImg);
+
+    // }
+
+    // function imgSizeInterval() {
+
+    //     setInterval(function() {
+
+    //         widthImg++;
+
+    //         getImgSize();
+
+    //     }, 1000);
+
+    // }
+
+
 
 
     $(function() {
@@ -44,6 +181,20 @@ $(document).ready(function() {
         $(".wrapper").css({"min-height" : $(window).height() + "px"});
 
         $(".wrapper").css({"padding-bottom" :  $(".footer").outerHeight(true) + "px"});
+
+    });
+
+    $(function() {
+
+        var indexSeoTxt;
+
+        $(".seo-text-section .links-box h3").click(function() {
+
+            indexSeoTxt = $(".seo-text-section .links-box h3").index(this);
+
+            $(".seo-text-section .links-box:eq(" + indexSeoTxt +")").toggleClass("active");
+
+        });
 
     });
 
@@ -116,21 +267,6 @@ $(document).ready(function() {
 
     });
     
-    
-
-
-
-    $(function() {
-
-        // var countHeadersRow = $(".header .header-row").length - 1;
-        // var countHeadersRowFor = 0;
-        // var headersRowsHeight = 0;
-
-        // getLogoBoxHeight();
-        // getLogoBoxHeight(countHeadersRow, countHeadersRowFor, headersRowsHeight);
-
-    });
-
 
 
     $(function() {
@@ -187,7 +323,6 @@ $(document).ready(function() {
 
         var showMosaicItemInterval = setInterval(function() {
 
-            // $(".mosaicflow__item:eq("+ mosaicItemsCountFor +")").fadeIn(200);
 
             $(".mosaicflow__item:eq("+ mosaicItemsCountFor +")").width($(".mosaicflow__item:eq("+ mosaicItemsCountFor +") .mosaic-inner-content").outerWidth(true));
 
@@ -203,7 +338,7 @@ $(document).ready(function() {
 
                 clearInterval(showMosaicItemInterval);
 
-                $(".mosaicflow").animate({"opacity" : 1}, 300);
+                // $(".mosaicflow").animate({"opacity" : 1}, 300);
 
             }
 
@@ -211,6 +346,9 @@ $(document).ready(function() {
 
 
     });
+
+
+
 
 
 // -----------------------------------------------------------------------------
@@ -234,6 +372,87 @@ $(document).ready(function() {
     });
 
 
+// -----------------------------------------------------------------------------
+
+// Адаптивное меню 
+
+
+    $(function() {
+
+        var indexRespNav;
+        var innerRespNavHeight;
+
+        $(".resp-nav .with-inner-nav").click(function(respNavLinkEvent) {
+
+            if( $(this).hasClass("with-inner-nav") ) {
+
+                respNavLinkEvent.preventDefault();
+
+                indexRespNav = $(".resp-nav li a").index(this);
+
+                innerRespNavHeight = $(".resp-nav li a:eq("+ indexRespNav +") + .inner-nav-hover > ul").outerHeight();
+
+                if( $(".resp-nav li a:eq("+ indexRespNav +") + .inner-nav-hover").height() <= 0) {
+
+                    $(".resp-nav li a:eq("+ indexRespNav +")").addClass("active");
+
+                    $(".resp-nav li a.with-inner-nav:eq("+ indexRespNav +") .fa-angle-dow").addClass("active");
+
+                    $(".resp-nav li a:eq("+ indexRespNav +") + .inner-nav-hover").animate({"height" : innerRespNavHeight + "px"}, 300);
+
+                } else {
+
+                    $(".resp-nav li a:eq("+ indexRespNav +")").removeClass("active");
+
+                    $(".resp-nav li a.with-inner-nav:eq("+ indexRespNav +") .fa-angle-dow").removeClass("active");
+
+                    $(".resp-nav li a:eq("+ indexRespNav +") + .inner-nav-hover").animate({"height" : 0 + "px"}, 300);
+
+                }
+
+
+            }
+
+        });
+
+
+    });
+
+
+    $(function() {
+
+        $(".respmenubtn").click(function() {
+
+            $(".resp-nav").toggleClass("respnavleftcoor");
+            $(".respmenubtn").toggleClass("respmenubtn-active");
+
+        });
+
+        $(".close-resp-nav").click(function() {
+
+            $(".resp-nav").toggleClass("respnavleftcoor");
+            $(".respmenubtn").toggleClass("respmenubtn-active");
+
+        });
+
+        $(".show-sidebar").click(function() {
+
+            $(".sidebar").addClass("show");
+
+        });
+
+        $(".close-sidebar-btn").click(function() {
+
+            console.log(".close-sidebar-btnr");
+
+            $(".sidebar").removeClass("show");
+
+        });
+
+
+    });
+
+
 
 // -----------------------------------------------------------------------------
 
@@ -252,7 +471,7 @@ $(document).ready(function() {
 
             $(".header .logo-box").height( $(".header").outerHeight(true) - headersRowsHeight);
 
-            console.log($(".header .center-box").height() ) ;
+            // console.log($(".header .center-box").height() ) ;
 
             // console.log ($(".header .center-box").offset().top );
 
